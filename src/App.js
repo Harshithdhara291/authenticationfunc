4f376ee73592809a3a6229fc9239b6af54cf3276
Harshithdhara291 <harshithdhara291@gmail.com>
2022-10-01 08:54:17 +0000

    cmt message

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import Home from './components/Home'
+import Login from './components/Login'
+import ProtectedRoute from './components/ProtectedRoute'
+import About from './components/About'
+import NotFound from './components/NotFound'
+
+import './App.css'
+
+/*  */
+
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/login" component={Login} />
+      <ProtectedRoute exact path="/" component={Home} />
+      <ProtectedRoute exact path="/about" component={About} />
+      <Route component={NotFound} />
+    </Switch>
+  </BrowserRouter>
+)
+
+export default App
